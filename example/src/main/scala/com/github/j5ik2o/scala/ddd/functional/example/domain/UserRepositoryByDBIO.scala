3863920632ba163f81bb2e9d414fcde10cba172b
Junichi Kato <j5ik2o@gmail.com>
2017-07-10 13:17:05 +0900

    refactor

--- example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByDBIO.scala
+++ example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByDBIO.scala
@@ -2,14 +2,8 @@ package com.github.j5ik2o.scala.ddd.functional.example.domain
 
 import com.github.j5ik2o.scala.ddd.functional.example.driver.slick3.UserSlickDBIODriver
 
-import scala.concurrent.ExecutionContext
-
 class UserRepositoryByDBIO(val driver: UserSlickDBIODriver) extends UserRepository {
-  override type IdValueType     = Long
-  override type AggregateIdType = UserId
-  override type AggregateType   = User
-  override type IOContextType   = ExecutionContext
-  override type DriverType      = UserSlickDBIODriver
+  override type DriverType = UserSlickDBIODriver
   import driver.profile.api._
   override type EvalType[A] = DBIO[A]
 }
--- example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByFuture.scala
+++ example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByFuture.scala
@@ -2,13 +2,9 @@ package com.github.j5ik2o.scala.ddd.functional.example.domain
 
 import com.github.j5ik2o.scala.ddd.functional.example.driver.slick3.UserSlickFutureDriver
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.Future
 
 class UserRepositoryByFuture(val driver: UserSlickFutureDriver) extends UserRepository {
-  override type IdValueType     = Long
-  override type AggregateIdType = UserId
-  override type AggregateType   = User
-  override type IOContextType   = ExecutionContext
-  override type EvalType[A]     = Future[A]
-  override type DriverType      = UserSlickFutureDriver
+  override type DriverType  = UserSlickFutureDriver
+  override type EvalType[A] = Future[A]
 }
