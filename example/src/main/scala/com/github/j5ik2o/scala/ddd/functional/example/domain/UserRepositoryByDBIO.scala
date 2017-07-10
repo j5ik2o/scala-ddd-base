e8db0c666d9c8eeb8fca0cf0deb1cce64e223d82
Junichi Kato <j5ik2o@gmail.com>
2017-07-10 13:17:45 +0900

    refactor

--- example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByDBIO.scala
+++ example/src/main/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositoryByDBIO.scala
@@ -3,7 +3,6 @@ package com.github.j5ik2o.scala.ddd.functional.example.domain
 import com.github.j5ik2o.scala.ddd.functional.example.driver.slick3.UserSlickDBIODriver
 
 class UserRepositoryByDBIO(val driver: UserSlickDBIODriver) extends UserRepository {
-  override type DriverType = UserSlickDBIODriver
-  import driver.profile.api._
-  override type EvalType[A] = DBIO[A]
+  override type DriverType  = UserSlickDBIODriver
+  override type EvalType[A] = driver.profile.api.DBIO[A]
 }
