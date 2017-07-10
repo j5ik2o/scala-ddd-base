6f604bd7a8d76ca6d70d72cc345e42d3357c8779
Junichi Kato <j5ik2o@gmail.com>
2017-07-10 18:44:02 +0900

    Modified AggregateFutureIOContext

--- core/src/main/scala/com/github/j5ik2o/scala/ddd/functional/AggregateFutureIOContext.scala
+++ core/src/main/scala/com/github/j5ik2o/scala/ddd/functional/AggregateFutureIOContext.scala
@@ -2,6 +2,6 @@ package com.github.j5ik2o.scala.ddd.functional
 
 import scala.concurrent.ExecutionContext
 
-trait AggregateFutureIOContext {
+trait AggregateFutureIOContext extends AggregateIOContext {
   val ec: ExecutionContext
 }
