81d9ce5bae7e0c5b369ae525a29091cf858ea458
Junichi Kato <j5ik2o@gmail.com>
2017-07-11 15:24:54 +0900

    refactor test

--- example/src/test/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositorySpec.scala
+++ example/src/test/scala/com/github/j5ik2o/scala/ddd/functional/example/domain/UserRepositorySpec.scala
@@ -26,10 +26,8 @@ class UserRepositorySpec
   "UserRepository" - {
     "should be able to store and resolve" - {
       "when DBIO of Slick" in {
-        val driver    = UserSlickDBIOStorageDriver(dbConfig.profile, dbConfig.db)
-        val evaluator = UserSlickDBIOEvaluator(driver)
-        val implicits = CatsDBIOImplicits(driver.profile)
-        import implicits._
+        val driver       = UserSlickDBIOStorageDriver(dbConfig.profile, dbConfig.db)
+        val evaluator    = UserSlickDBIOEvaluator(driver)
         implicit val ctx = SlickFutureIOContext(ec)
         val program = for {
           _  <- UserRepository.store(User(UserId(1), "kato"))
@@ -37,37 +35,39 @@ class UserRepositorySpec
           _  <- UserRepository.deleteById(UserId(1))
           r2 <- UserRepository.resolveBy(UserId(1))
         } yield (r1, r2)
+        val implicits = CatsDBIOImplicits(driver.profile)
+        import implicits._
         val dbio   = evaluator.run(program)
         val future = driver.db.run(dbio)
         val result = future.futureValue
         println(result)
       }
       "when Future of Slick" in {
-        val driver       = UserSlickFutureStorageDriver(dbConfig.profile, dbConfig.db)
-        val evaluator    = UserSlickFutureEvaluator(driver)
-        implicit val ctx = SlickFutureIOContext(ec)
+        val driver    = UserSlickFutureStorageDriver(dbConfig.profile, dbConfig.db)
+        val evaluator = UserSlickFutureEvaluator(driver)
         val program = for {
           _  <- UserRepository.store(User(UserId(2), "kato"))
           r1 <- UserRepository.resolveBy(UserId(2))
           _  <- UserRepository.deleteById(UserId(2))
           r2 <- UserRepository.resolveBy(UserId(2))
         } yield (r1, r2)
-        val future = evaluator.run(program)
-        val result = future.futureValue
+        implicit val ctx = SlickFutureIOContext(ec)
+        val future       = evaluator.run(program)
+        val result       = future.futureValue
         println(result)
       }
       "when Future of Skinny" in {
-        val driver       = UserSkinnyORMFutureStorageDriver()
-        val evaluator    = UserSkinnyORMFutureEvaluator(driver)
-        implicit val ctx = SkinnyORMFutureIOContext(ec, AutoSession)
+        val driver    = UserSkinnyORMFutureStorageDriver()
+        val evaluator = UserSkinnyORMFutureEvaluator(driver)
         val program = for {
           _  <- UserRepository.store(User(UserId(3), "kato"))
           r1 <- UserRepository.resolveBy(UserId(3))
           _  <- UserRepository.deleteById(UserId(3))
           r2 <- UserRepository.resolveBy(UserId(3))
         } yield (r1, r2)
-        val future = evaluator.run(program)
-        val result = future.futureValue
+        implicit val ctx = SkinnyORMFutureIOContext(ec, AutoSession)
+        val future       = evaluator.run(program)
+        val result       = future.futureValue
         println(result)
       }
     }
