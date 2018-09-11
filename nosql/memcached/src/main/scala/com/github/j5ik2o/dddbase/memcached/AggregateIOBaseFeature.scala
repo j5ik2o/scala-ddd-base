d3591e70bd4adcaec105b6329bce8add08f8364a
Junichi Kato <j5ik2o@gmail.com>
2018-09-11 14:20:55 +0900

    Modify the AggregateIdType constraint

--- nosql/memcached/src/main/scala/com/github/j5ik2o/dddbase/memcached/AggregateIOBaseFeature.scala
+++ nosql/memcached/src/main/scala/com/github/j5ik2o/dddbase/memcached/AggregateIOBaseFeature.scala
@@ -1,15 +1,14 @@
 package com.github.j5ik2o.dddbase.memcached
 
 import cats.data.ReaderT
+import com.github.j5ik2o.dddbase.AggregateIO
 import com.github.j5ik2o.dddbase.memcached.AggregateIOBaseFeature.RIO
-import com.github.j5ik2o.dddbase.{ AggregateIO, AggregateLongId }
 import com.github.j5ik2o.reactive.memcached.MemcachedConnection
 import monix.eval.Task
 
 import scala.concurrent.duration.Duration
 
 trait AggregateIOBaseFeature extends AggregateIO[RIO] {
-  override type IdType <: AggregateLongId
   type RecordType <: MemcachedDaoSupport#Record
   type DaoType <: MemcachedDaoSupport#Dao[RecordType]
 
