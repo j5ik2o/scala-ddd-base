9b432afb731ec0f9eb53d5c77c57ba648920bd1f
Junichi Kato <j5ik2o@gmail.com>
2018-09-11 14:20:55 +0900

    Modify the AggregateIdType constraint

--- nosql/memory/src/main/scala/com/github/j5ik2o/dddbase/memory/AggregateIOBaseFeature.scala
+++ nosql/memory/src/main/scala/com/github/j5ik2o/dddbase/memory/AggregateIOBaseFeature.scala
@@ -1,11 +1,10 @@
 package com.github.j5ik2o.dddbase.memory
 
+import com.github.j5ik2o.dddbase.AggregateIO
 import com.github.j5ik2o.dddbase.memory.AggregateIOBaseFeature.RIO
-import com.github.j5ik2o.dddbase.{ AggregateIO, AggregateLongId }
 import monix.eval.Task
 
 trait AggregateIOBaseFeature extends AggregateIO[RIO] {
-  override type IdType <: AggregateLongId
   type RecordType <: MemoryDaoSupport#Record
   type DaoType <: MemoryDaoSupport#Dao[RecordType]
 
--- nosql/redis/src/main/scala/com/github/j5ik2o/dddbase/redis/AggregateIOBaseFeature.scala
+++ nosql/redis/src/main/scala/com/github/j5ik2o/dddbase/redis/AggregateIOBaseFeature.scala
@@ -1,15 +1,14 @@
 package com.github.j5ik2o.dddbase.redis
 
 import cats.data.ReaderT
+import com.github.j5ik2o.dddbase.AggregateIO
 import com.github.j5ik2o.dddbase.redis.AggregateIOBaseFeature.RIO
-import com.github.j5ik2o.dddbase.{ AggregateIO, AggregateLongId }
 import com.github.j5ik2o.reactive.redis.RedisConnection
 import monix.eval.Task
 
 import scala.concurrent.duration.Duration
 
 trait AggregateIOBaseFeature extends AggregateIO[RIO] {
-  override type IdType <: AggregateLongId
   type RecordType <: RedisDaoSupport#Record
   type DaoType <: RedisDaoSupport#Dao[RecordType]
 
