7c02b94f5b53761007936f7ea4846579531def42
Junichi Kato <j5ik2o@gmail.com>
2019-02-21 13:41:15 +0900

    Update reactive-dynamodb version.

--- example/src/test/scala/com/github/j5ik2o/dddbase/example/repository/dynamodb/UserAccountRepositoryOnDynamoDBSpec.scala
+++ example/src/test/scala/com/github/j5ik2o/dddbase/example/repository/dynamodb/UserAccountRepositoryOnDynamoDBSpec.scala
@@ -2,50 +2,33 @@ package com.github.j5ik2o.dddbase.example.repository.dynamodb
 
 import java.net.URI
 import java.time.ZonedDateTime
-import java.util.UUID
 
-import com.amazonaws.auth.{ AWSStaticCredentialsProvider, BasicAWSCredentials }
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.dynamodbv2.{ AmazonDynamoDB, AmazonDynamoDBClientBuilder }
 import com.github.j5ik2o.dddbase.example.model._
 import com.github.j5ik2o.dddbase.example.repository.IdGenerator
 import com.github.j5ik2o.reactive.dynamodb.model._
 import com.github.j5ik2o.reactive.dynamodb.monix.DynamoDBTaskClientV2
-import com.github.j5ik2o.reactive.dynamodb.{ DynamoDBAsyncClientV2, DynamoDBSpecSupport }
+import com.github.j5ik2o.reactive.dynamodb.{ DynamoDBAsyncClientV2, DynamoDBEmbeddedSpecSupport }
 import monix.execution.Scheduler.Implicits.global
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{ FreeSpec, Matchers }
 import software.amazon.awssdk.auth.credentials.{ AwsBasicCredentials, StaticCredentialsProvider }
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 
 import scala.concurrent.duration._
 
-class UserAccountRepositoryOnDynamoDBSpec extends FreeSpec with Matchers with DynamoDBSpecSupport {
+class UserAccountRepositoryOnDynamoDBSpec
+    extends FreeSpec
+    with Matchers
+    with ScalaFutures
+    with DynamoDBEmbeddedSpecSupport {
   implicit val pc: PatienceConfig = PatienceConfig(20 seconds, 1 seconds)
 
-  lazy val accessKeyId     = "x"
-  lazy val secretAccessKey = "x"
-  lazy val endpoint        = s"http://127.0.0.1:$port"
-
-  override protected def dynamoDbClient: AmazonDynamoDB =
-    AmazonDynamoDBClientBuilder
-      .standard()
-      .withCredentials(
-        new AWSStaticCredentialsProvider(
-          new BasicAWSCredentials(accessKeyId, secretAccessKey)
-        )
-      )
-      .withEndpointConfiguration(
-        new EndpointConfiguration(endpoint, Regions.AP_NORTHEAST_1.getName)
-      )
-      .build()
-
-  val underlying = DynamoDbAsyncClient
+  val underlying: DynamoDbAsyncClient = DynamoDbAsyncClient
     .builder()
     .credentialsProvider(
       StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey))
     )
-    .endpointOverride(URI.create(endpoint))
+    .endpointOverride(URI.create(dynamoDBEndpoint))
     .build()
 
   val userAccount = UserAccount(
@@ -72,7 +55,7 @@ class UserAccountRepositoryOnDynamoDBSpec extends FreeSpec with Matchers with Dy
         updatedAt = Some(ZonedDateTime.now)
       )
 
-  val client = DynamoDBTaskClientV2(DynamoDBAsyncClientV2(underlying))
+  val client: DynamoDBTaskClientV2 = DynamoDBTaskClientV2(DynamoDBAsyncClientV2(underlying))
 
   "UserAccountRepositoryOnDynamoDB" - {
     "store" in {
